8fa942dcdd55fd958106034a3ed4c54ff14d77b6
Pavel <48726480+pgavriluk@users.noreply.github.com>
2021-06-05 16:20:59 -0700

    Create setTimeout.js

---
+++ setTimeout.js
@@ -0,0 +1,14 @@
+javascript: (function() {
+    max = prompt('how many?');
+    if (max) {
+        for (x = 1; x <= max; x++) {
+            setTimeout(function(name) {
+                document.querySelector('input.new-todo').value = name;
+                document.querySelector('input.new-todo').dispatchEvent(new Event('change',{
+                    'bubble': true
+                }));
+            }, x * 100, "todo " + x)
+        }
+    }
+}
+)()
